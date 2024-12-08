4391433c6148fa494fb5667ce7f91119b067d2ae
CutoffSquire917 <pushkin080109@gmail.com>
2024-12-08 15:09:50 +0100

    'Finaly'

--- hw-26/Source.cpp
+++ hw-26/Source.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "Queue.h"
 using namespace std;
 
 int main()
 {
-
+	Queue<string>* q1 = new Queue<string>();
+	q1->Enqueue(MyValue<string>("Hello world!"));
+	cout << q1->GetFirst().GetValue() << endl;
+	q1->Dequeue();
+	delete q1;
 }
